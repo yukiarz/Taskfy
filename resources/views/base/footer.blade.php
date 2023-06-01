b7176fbea1c411c7bdc57eb83581de30fe48bbaa
ezyfa <marzd000@gmail.com>
2023-06-01 13:29:15 +0700

    update

--- resources/views/base/footer.blade.php
+++ resources/views/base/footer.blade.php
@@ -10,22 +10,6 @@
                   </script>
                   Made with ❤️ by
                   <a href=#" class="footer-link fw-bolder">ゆき</a>
-                  <!-- <a href="https://themeselection.com/license/" class="footer-link me-4" target="_blank">License</a>
-                  <a href="https://themeselection.com/" target="_blank" class="footer-link me-4">More Themes</a>
-
-                  <a
-                    href="https://themeselection.com/demo/sneat-bootstrap-html-admin-template/documentation/"
-                    target="_blank"
-                    class="footer-link me-4"
-                    >Documentation</a
-                  >
-
-                  <a
-                    href="https://github.com/themeselection/sneat-html-admin-template-free/issues"
-                    target="_blank"
-                    class="footer-link me-4"
-                    >Support</a
-                  > -->
                 </div>
               </div>
             </footer>
